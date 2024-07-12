cf8756446b4b2e75dcdbb8dc028dc88d838acd6e
fjuza1 <fjuza@ditec.sk>
2024-07-12 09:58:04 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,8 @@
 const { defineConfig } = require("cypress");
-const moment = require('moment');
+const moment = require('moment')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('task',{
-
-       }
-    )
     },
   },
 });
